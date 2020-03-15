cf0ecf0d1f140122a3482dcab68cf5428da292fa
Michael <michael@marolt.com>
2020-03-15 21:52:42 +0100

    TCP connection first prototype

--- einzelabgabe_MaroltMichael/app/src/main/java/com/example/einzelabgabe_maroltmichael/MainActivity.java
+++ einzelabgabe_MaroltMichael/app/src/main/java/com/example/einzelabgabe_maroltmichael/MainActivity.java
@@ -14,18 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //new Thread(new TCP_connection()).start();
+
+
     }
 
 
     public void sendMessage(View view) {
+
         EditText editNumber= findViewById(R.id.editNumber);
         String txt = editNumber.getText().toString();
         TextView tv = findViewById(R.id.server_response);
 
         try {
-            Log.e("error", editNumber.toString());
-            String message = TCP_connection.getMessage(txt);
-            tv.setText(message);
+            //String message = TCP_connection.getMessage(txt);
+            //tv.setText(message);
+
+            TCP_Thread t = new TCP_Thread(txt);
+            t.start();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                //TODO do sth
+            }
+            tv.setText(t.getModifiedSentence());
+
         } catch (Exception e) {
             tv.setText(e.toString());
         }
---
+++ einzelabgabe_MaroltMichael/app/src/main/java/com/example/einzelabgabe_maroltmichael/TCP_Thread.java
@@ -0,0 +1,63 @@
+package com.example.einzelabgabe_maroltmichael;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TCP_Thread extends Thread {
+    static Socket clientSocket = null;
+    private static final int SERVERPORT = 53212;
+    private static final String SERVER = "se2-isys.aau.at";
+    private String message;
+    private String modifiedSentence;
+
+    TCP_Thread(String message) {
+        this.message = message;
+    }
+
+    public String getModifiedSentence() {
+        return this.modifiedSentence;
+    }
+
+
+
+
+
+
+    @Override
+    public void run() {
+        try {
+            clientSocket = new Socket(SERVER, SERVERPORT);
+
+            try {
+                //clientSocket = new Socket("se2-isys.aau.at",53212);
+
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                outToServer.writeBytes(message + "\n");
+
+                modifiedSentence = inFromServer.readLine();
+
+                System.out.println("FROM SERVER: " + modifiedSentence);
+
+            } catch (Exception e) {
+                System.out.println(e.toString());
+                throw e;
+            }  finally {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            }
+
+        } catch (UnknownHostException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+}
