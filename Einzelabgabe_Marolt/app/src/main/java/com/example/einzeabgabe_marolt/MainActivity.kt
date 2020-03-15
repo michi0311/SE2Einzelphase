da6dc342b89d4ca985fe5260bf3e9b848be094fd
Michael <michael@marolt.com>
2020-03-15 14:53:45 +0100

    New Project Init
    the first project was with Kotlin

--- Einzelabgabe_Marolt/app/src/main/java/com/example/einzeabgabe_marolt/MainActivity.kt
+++ Einzelabgabe_Marolt/app/src/main/java/com/example/einzeabgabe_marolt/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.einzeabgabe_marolt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendRequest(view: View?) { // Do something in response to button
+        
     }
 }
---
+++ einzelabgabe_MaroltMichael/app/src/main/java/com/example/einzelabgabe_maroltmichael/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.einzelabgabe_maroltmichael;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
